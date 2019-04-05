ea7c9693ea0a4ce8998529672e41f47035958259
Jean Santos <jeansantosc@gmail.com>
2019-04-04 23:05:38 -0300

    add theme for styled components

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+export default {
+    colors: {
+        primaryLight: '#7ed56f',
+        primaryMedium: '#55c57a',
+        primaryDark: '#28b485',
+        background: '#FFFFFF',
+        transparent: 'rgba(0,0,0,0)',
+        gray: '#777',
+    },
+    fontSize: {
+        h1: '42px',
+        h2: '33px',
+        h3: '20px',
+        h4: '18px',
+        h5: '16px',
+        subtitle: '18px',
+        normal: '16px',
+        small: '12px',
+    },
+    fontWeight: {
+        thin: '100',
+        light: '300',
+        regular: '400',
+        bold: '700',
+        black: '900',
+    },
+    fontFamily:
+        'Lato, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+};
